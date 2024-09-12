5f46ae1d86ba2ca3650b765aa6c9f3b2e962ce13
Rostyslav Mukha <snowyfl1cker@gmail.com>
2024-09-12 20:00:57 +0300

    Create a template for MVC #PK-4

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_K.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
