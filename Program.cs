8a9b896a23fc9d1f1ddc0226233f6b9a10ecc6f0
Rostyslav Mukha <snowyfl1cker@gmail.com>
2024-09-15 20:47:23 +0300

    PK-8 Connected SwaggerUI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Project_K.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,17 @@ builder.Services.AddDbContext<KurinDbContext>(options =>
         new MySqlServerVersion(new Version(9, 0, 1))
     ));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API Documentation",
+        Description = "ASP.NET Core Web API with Swagger"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +32,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Documentation"));
 }
 
 app.UseHttpsRedirection();
