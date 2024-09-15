85bac1a34ac3aca820b2328204e0640f31ccb76a
Rostyslav Mukha <snowyfl1cker@gmail.com>
2024-09-15 21:42:11 +0300

    PK-9 Created a Controller with CRUD operations

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_K.Data;
+using Project_K.Models;
+
+namespace Project_K.Controllers
+{
+    public class MembersController : ControllerBase
+    {
+        private readonly KurinDbContext _context;
+
+        public MembersController(KurinDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("api/members")]
+        public async Task<IActionResult> GetMembers()
+        {
+            var members = await _context.Members.ToListAsync();
+            return Ok(members);
+        }
+
+        [HttpGet]
+        [Route("api/members/{id}")]
+        public async Task<IActionResult> GetMember(int id)
+        {
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return Ok(member);
+        }
+
+        [HttpPost]
+        [Route("api/members")]
+        public async Task<IActionResult> CreateMember([FromBody] Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Members.Add(member);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMember), new { id = member.Id }, member);
+        }
+
+        [HttpPut]
+        [Route("api/members/{id}")]
+        public async Task<IActionResult> UpdateMember(int id, [FromBody] Member member)
+        {
+            if (id != member.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(member).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Members.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("api/members/{id}")]
+        public async Task<IActionResult> DeleteMember(int id)
+        {
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            _context.Members.Remove(member);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
