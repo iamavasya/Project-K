b055eefa0899ee0d8f7e64ec9cc81a18efe1dde5
Rostyslav Mukha <snowyfl1cker@gmail.com>
2024-09-16 19:14:11 +0300

    PK-23 Updated DB and Models

--- Models/MemberLevel.cs
+++ Models/MemberLevel.cs
@@ -1,10 +1,20 @@
-﻿namespace Project_K.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_K.Models
 {
     public class MemberLevel
     {
         public int Id { get; set; }
+
+        [ForeignKey(nameof(Member))]
+        public int MemberId { get; set; }
         public Member Member { get; set; }
+
+        [ForeignKey(nameof(Level))]
+        public int LevelId { get; set; }
         public Level Level { get; set; }
-        public required DateOnly AchieveDate { get; set; }
+        [Required]
+        public DateOnly AchieveDate { get; set; }
     }
 }
