684143ea57e4bc79fe704ae11bdf7f07afa21298
Rostyslav Mukha <64521547+iamavasya@users.noreply.github.com>
2024-09-16 20:27:31 +0300

    Revert "PK-11 PK-27 input output forms"

--- Program.cs
+++ Program.cs
@@ -1,30 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Project_K.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
-
-builder.Services.AddDbContext<KurinDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(9, 0, 1))
-    ));
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "API Documentation",
-        Description = "ASP.NET Core Web API with Swagger"
-    });
-});
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -34,10 +11,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-} else
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Documentation"));
 }
 
 app.UseHttpsRedirection();
