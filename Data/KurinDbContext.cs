30b398f6e6a45259fc1c893172b7bb4a7f7ed3eb
Rostyslav Mukha <snowyfl1cker@gmail.com>
2024-09-19 14:05:40 +0300

    PK-30 Added Google Places API

--- Data/KurinDbContext.cs
+++ Data/KurinDbContext.cs
@@ -8,12 +8,10 @@ namespace Project_K.Data
         public KurinDbContext(DbContextOptions<KurinDbContext> options) : base(options)
         {
         }
-
-        public DbSet<Address> Addresses { get; set; }
+        
         public DbSet<KurinLevel> KurinLevels { get; set; }
         public DbSet<Level> Levels { get; set; }
         public DbSet<Member> Members  { get; set; }
         public DbSet<MemberLevel> MemberLevels { get; set; }
-        public DbSet<School> Schools { get; set; }
     }
 }
