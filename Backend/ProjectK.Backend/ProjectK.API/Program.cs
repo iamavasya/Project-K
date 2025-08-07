5312ba717bc36be6e8ac3ec122c7fe5393ba50d3
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-08-08 01:04:51 +0300

    Add Swagger and KurinController

--- Backend/ProjectK.Backend/ProjectK.API/Program.cs
+++ Backend/ProjectK.Backend/ProjectK.API/Program.cs
@@ -1,17 +1,52 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ProjectK.API.MappingProfiles.KurinModule;
+using ProjectK.BusinessLogic.Modules.Kurin.Queries.Handlers;
+using ProjectK.Infrastructure.DbContexts;
 
 namespace ProjectK.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                );
+            });
+
             builder.Services.AddAutoMapper(cfg => { }, typeof(KurinProfile));
+            builder.Services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(typeof(GetKurinByKeyQueryHandler).Assembly)
+            );
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddProjectDependencies();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                await dbContext.Database.MigrateAsync();
+            }
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+            app.MapControllers();
+
+            app.MapGet("/", () => "Backend Started");
 
             app.Run();
         }
---
+++ Backend/ProjectK.Backend/ProjectK.API/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectK.Common.Interfaces;
+using ProjectK.Common.Interfaces.Modules.KurinModule;
+using ProjectK.Infrastructure.Repositories;
+using ProjectK.Infrastructure.UnitOfWork;
+
+namespace ProjectK.API
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddProjectDependencies(this IServiceCollection services)
+        {
+            // Services
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            // Repositories
+            services.AddScoped<IKurinRepository, KurinRepository>();
+            return services;
+        }
+    }
+}
