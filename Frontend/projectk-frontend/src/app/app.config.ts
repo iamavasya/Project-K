67d8f7fbb890c07d419ebb3b603e2a14ff5f4c67
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-08-12 01:56:23 +0300

    Add frontend project

---
+++ Frontend/projectk-frontend/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+          preset: Aura
+        }
+    })
+  ]
+};
