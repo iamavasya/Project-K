995069995aa371378d0fb2064ca15a5c3b0a59d8
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-08-12 23:31:36 +0300

    Update base frontend tests

--- Frontend/projectk-frontend/src/app/features/kurinModule/common/services/kurin-service/kurin.service.spec.ts
+++ Frontend/projectk-frontend/src/app/features/kurinModule/common/services/kurin-service/kurin.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { KurinService } from './kurin.service';
 
 describe('KurinService', () => {
   let service: KurinService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        KurinService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(KurinService);
   });
 
