6ea73a5c7f73f8e9c11e0b30202e86c971a26fbe
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-09-16 12:31:29 +0300

    Refactor member controller / Fix Swagger 500 error

---
+++ Backend/ProjectK.Backend/ProjectK.Common/Models/Dtos/Requests/UpsertMemberRequest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectK.Common.Models.Dtos.Requests
+{
+    public class UpsertMemberRequest
+    {
+        public Guid? MemberKey { get; set; }
+        public Guid GroupKey { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public bool? RemoveProfilePhoto { get; set; }
+        public IFormFile? Blob { get; set; }
+    }
+}
