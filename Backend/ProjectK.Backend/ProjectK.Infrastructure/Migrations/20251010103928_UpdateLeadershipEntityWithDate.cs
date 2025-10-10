080369f402172e920cab05f4ca02d5cdcb1737d7
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-10-10 13:40:42 +0300

    Add Leadership repository

---
+++ Backend/ProjectK.Backend/ProjectK.Infrastructure/Migrations/20251010103928_UpdateLeadershipEntityWithDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectK.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLeadershipEntityWithDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "EndDate",
+                table: "Leaderships",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "StartDate",
+                table: "Leaderships",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Leaderships");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Leaderships");
+        }
+    }
+}
