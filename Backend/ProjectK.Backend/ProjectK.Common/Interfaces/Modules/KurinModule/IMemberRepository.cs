6fcf779ed915db2a27ddaa5ee94dbfc198aa0cf2
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-11-20 18:55:15 +0200

    Resolve sonar comments

--- Backend/ProjectK.Backend/ProjectK.Common/Interfaces/Modules/KurinModule/IMemberRepository.cs
+++ Backend/ProjectK.Backend/ProjectK.Common/Interfaces/Modules/KurinModule/IMemberRepository.cs
@@ -10,14 +10,14 @@ namespace ProjectK.Common.Interfaces.Modules.KurinModule
 {
     public interface IMemberRepository : IBaseEntityRepository<Member>
     {
-        Task<IEnumerable<Member>> GetAllAsync(Guid groupKey, CancellationToken token = default);
-        Task<IEnumerable<Member>> GetAllByKurinKeyAsync(Guid kurinKey, CancellationToken token = default);
+        Task<IEnumerable<Member>> GetAllAsync(Guid groupKey, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Member>> GetAllByKurinKeyAsync(Guid kurinKey, CancellationToken cancellationToken = default);
 
         #region PlastLevelHistory Methods
-        Task AddPlastLevelHistoryAsync(Guid memberKey, PlastLevelHistory history, CancellationToken token = default);
-        Task UpdatePlastLevelHistoryAsync(Guid memberKey, PlastLevelHistory updatedHistory, CancellationToken token = default);
-        Task RemovePlastLevelHistoryAsync(Guid memberKey, Guid historyKey, CancellationToken token = default);
-        Task<IEnumerable<PlastLevelHistory>> GetPlastLevelHistoryAsync(Guid memberKey, CancellationToken token = default);
+        Task AddPlastLevelHistoryAsync(Guid memberKey, PlastLevelHistory history, CancellationToken cancellationToken = default);
+        Task UpdatePlastLevelHistoryAsync(Guid memberKey, PlastLevelHistory updatedHistory, CancellationToken cancellationToken = default);
+        Task RemovePlastLevelHistoryAsync(Guid memberKey, Guid historyKey, CancellationToken cancellationToken = default);
+        Task<IEnumerable<PlastLevelHistory>> GetPlastLevelHistoryAsync(Guid memberKey, CancellationToken cancellationToken = default);
         #endregion
 
         #region LeadershipHistory Methods
