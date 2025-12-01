3ea8cbde3bf5178faa53e7db7aba4ae4eb9ca627
Rostyslav Mukha <snowyfl1cker@gmail.com>
2025-12-01 13:49:08 +0200

    Remove comments, improve some strings and logic

--- Frontend/projectk-frontend/src/app/features/kurinModule/common/models/enums/roleWeight.enum.ts
+++ Frontend/projectk-frontend/src/app/features/kurinModule/common/models/enums/roleWeight.enum.ts
@@ -6,8 +6,8 @@ export enum RoleWeight {
 }
 
 export const RoleWeightOptions = [
-  { label: 'Не важливий (0)', value: RoleWeight.NotImportant },
-  { label: 'Низький (1.0)', value: RoleWeight.Low },
-  { label: 'Середній (3.0)', value: RoleWeight.Medium },
-  { label: 'Високий (5.0)', value: RoleWeight.High },
+  { label: 'Неважливий', value: RoleWeight.NotImportant },
+  { label: 'Низький', value: RoleWeight.Low },
+  { label: 'Середній', value: RoleWeight.Medium },
+  { label: 'Високий', value: RoleWeight.High },
 ];
--- Frontend/projectk-frontend/src/app/features/kurinModule/common/models/planningSessionDto.ts
+++ Frontend/projectk-frontend/src/app/features/kurinModule/common/models/planningSessionDto.ts
@@ -1,5 +1,5 @@
 export interface DateRangeDto {
-  start: string; // ISO String ("2025-06-01")
+  start: string;
   end: string;
 }
 
@@ -15,17 +15,14 @@ export interface PlanningSessionDto {
   name: string;
   kurinKey: string;
 
-  // Параметри
-  searchStart: string; // ISO String
+  searchStart: string;
   searchEnd: string;
   durationDays: number;
 
-  // Результат
-  isCalculated: boolean; // Використовуємо це замість статусу 'New' | 'Calculated'
+  isCalculated: boolean;
   optimalStartDate?: string | null;
   optimalEndDate?: string | null;
   conflictScore: number;
 
-  // Для списку цей масив може бути пустим, для деталей - повним
   participants: PlanningParticipantDto[];
 }
