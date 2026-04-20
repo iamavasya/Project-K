aefd3cc63df0a0b3e941309aa8ae09bf9e0b6308
Rostyslav Mukha <snowyfl1cker@gmail.com>
2026-04-20 12:09:05 +0300

    Add local tailscale environment

--- Frontend/projectk-frontend/src/app/features/kurinModule/common/services/probes-and-badges/probes-catalog.service.ts
+++ Frontend/projectk-frontend/src/app/features/kurinModule/common/services/probes-and-badges/probes-catalog.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../../../environments/environment';
 import { GroupedProbeDto } from '../../models/probes-and-badges/groupedProbeDto';
 import { ProbeSummaryDto } from '../../models/probes-and-badges/probeSummaryDto';
 
---
+++ Frontend/projectk-frontend/src/environments/environment.tailscale.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://100.64.66.7:5205/api'
+};
